f2b99f7da4eac5191da1c3e4f4f28781445566ca
Vaheed Sadeghi <vaheeds@live.com>
2017-06-06 09:18:18 +0430

    Done!

--- VillageCode/Form1.Designer.cs
+++ VillageCode/Form1.Designer.cs
@@ -28,12 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(199, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "2 >>>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(19, 53);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(231, 290);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(284, 53);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(231, 290);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(548, 53);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(231, 290);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(464, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "3 >>>";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "1- Paste Farsi List...";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(281, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Latin List...";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(545, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "3 Code...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(791, 380);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Generate Unique Code From Farsi String";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- VillageCode/Form1.cs
+++ VillageCode/Form1.cs
@@ -11,9 +11,108 @@ namespace VillageCode
 {
     public partial class Form1 : Form
     {
+        Dictionary<char, char> alphabet = new Dictionary<char, char>();
+        public void FillAlphabet()
+        {
+            alphabet.Clear();
+            //alphabet.Add('ا', 'A');
+            alphabet.Add('آ', 'A');
+            alphabet.Add('ب', 'B');
+            alphabet.Add('پ', 'P');
+            alphabet.Add('ت', 'T');
+            alphabet.Add('ث', 'S');
+            alphabet.Add('ج', 'J');
+            alphabet.Add('چ', 'C');
+            alphabet.Add('ح', 'H');
+            alphabet.Add('خ', 'K');
+            alphabet.Add('د', 'D');
+            alphabet.Add('ذ', 'Z');
+            alphabet.Add('ر', 'R');
+            alphabet.Add('ز', 'Z');
+            alphabet.Add('ژ', 'Z');
+            alphabet.Add('س', 'S');
+            alphabet.Add('ش', 'S');
+            alphabet.Add('ص', 'S');
+            alphabet.Add('ض', 'Z');
+            alphabet.Add('ط', 'T');
+            alphabet.Add('ظ', 'Z');
+            alphabet.Add('ع', 'A');
+            alphabet.Add('غ', 'G');
+            alphabet.Add('ف', 'F');
+            alphabet.Add('ق', 'Q');
+            alphabet.Add('ک', 'K');
+            alphabet.Add('ك', 'K');
+            alphabet.Add('گ', 'G');
+            alphabet.Add('ل', 'L');
+            alphabet.Add('م', 'M');
+            alphabet.Add('ن', 'N');
+            alphabet.Add('و', 'V');
+            alphabet.Add('ه', 'H');
+            alphabet.Add('ی', 'Y');
+            alphabet.Add('ي', 'Y');
+        }
         public Form1()
         {
             InitializeComponent();
+            FillAlphabet();
+        }
+        List<string> converted = new List<string>();
+        List<string> shortlist = new List<string>();
+        string temp = "";
+        char[] temp2 = "xxx".ToCharArray();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char tempCh;
+            foreach (string item in textBox1.Text.Split('\n'))
+            {
+                foreach (char ch in item.ToCharArray())
+                {
+                    if (alphabet.TryGetValue(ch, out tempCh))
+                    {
+                        temp += tempCh;
+                    }
+                }
+                converted.Add(temp);
+                temp = "";
+            }
+            converted.ForEach(s => textBox2.Text += (s + "\r\n"));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (string s in converted)
+            {
+                if (string.IsNullOrWhiteSpace(s)) continue;
+               
+                temp2[0] = s[0];
+
+                temp2[1] = s.Length > 1 ? s[1] : 'x';
+
+                temp2[2] = s.Length > 2 ? s[2] : 'x';
+                if (!shortlist.Contains(new String(temp2)))
+                    shortlist.Add(new String(temp2));
+                else
+                {
+                    temp2[0] = s[0];
+                    
+                    for (int i = 3; i < s.Length; i++)
+                    {
+                        temp2[2] = s[i];
+                        if (!shortlist.Contains(new String(temp2))) break;
+                        for (int j = 2; j < s.Length; j++)
+                        {                           
+                            temp2[1] = s[j];
+                            if (!shortlist.Contains(new String(temp2))) break;
+                        }
+                        
+                    }
+                    if (!shortlist.Contains(new String(temp2)))
+                        shortlist.Add(new String(temp2));
+                    else
+                        shortlist.Add("xxx");
+                }                
+            }
+            shortlist.ForEach(s => textBox3.Text += (s + "\r\n"));
         }
     }
 }
